6877b139ec967350a1991c5a83a0b016d1528465
pomidorka2010 <lamc560ki@hotmail.com>
2025-08-15 12:47:09 +0200

    feat: initial commit for routes/user.js

---
+++ routes/user.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+
+// Get user by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send('User not found');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Update user profile
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) return res.status(404).send('User not found');
+    res.json(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Delete user profile
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send('User not found');
+    res.send('User deleted');
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
