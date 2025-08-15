c6c69d96cbc5e90f5a96900b3a7302920323fad6
pomidorka2010 <lamc560ki@hotmail.com>
2025-08-15 12:47:06 +0200

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/api', authRoutes);
+app.use('/api/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
